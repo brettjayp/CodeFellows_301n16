0881248e1d9d151b94914ecdf563e896fb04e746
Brett Packard <brettjayp@gmail.com>
2020-01-10 20:47:58 -0800

    prepped lab 3

---
+++ Labs/lab-03/README.md
@@ -0,0 +1,61 @@
+# LAB 02 301n16
+
+# Feature one 
+Number and name of feature: #1: Display images
+
+Navigator: Brett
+
+Driver: Henok
+
+Estimate of time needed to complete: Fourty Minutes
+
+Start time: 7:20pm
+
+Finish time: 9:15pm
+
+Actual time needed to complete: 1:55
+
+# Feature two
+Number and name of feature: #2: Filter Images
+
+Navigator: Henok
+
+Driver: Brett
+
+Estimate of time needed to complete: One hour, ten minuts
+
+Start time: 6:40pm
+
+Finish time: 9:15pm
+
+Actual time needed to complete: 2:35
+
+# Feature three
+Number and name of feature: #3: Style the Application
+
+Navigator: Joint effort
+
+Driver: Joint effort
+
+Estimate of time needed to complete: 2 hours
+
+Start time: 5:20 pm
+
+Finish time: 6:20 pm
+
+Actual time needed to complete: 1 hour
+
+<!-- Template of time estimate format -->
+Number and name of feature: ________________________________
+
+Navigator: __________
+
+Driver: __________
+
+Estimate of time needed to complete: _____
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ Labs/lab-03/js/app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+
+function Image(img) {
+  this.url = img.image_url,
+    this.title = img.title,
+    this.description = img.description,
+    this.keyword = img.keyword,
+    this.horns = img.horns;
+}
+
+Image.allImages = [];
+
+
+let keywords = [];
+Image.prototype.options = function() {
+  Image.allImages.forEach(value => {
+    if(keywords.indexOf(value.keyword) > -1){
+    } else {
+      keywords.push(value.keyword);
+    };
+  });
+
+  keywords.forEach(value => $('select').append(`<option id="option_${value}">${value}</option>`));
+
+  $('select').on('change', (event) => {
+    console.log(event.target.value);
+    let option = event.target.value;
+      $('div').hide();
+      $(`.${option}`).show();
+  })
+};
+
+Image.prototype.render = function () {
+  $('main').append('<div class = "template"></div>');
+  let imageTemplate = $('div[class = "template"]');
+  let imageHtml = $('#photo-template').html();
+  imageTemplate.html(imageHtml);
+
+  imageTemplate.find('h2').text(this.title);
+  imageTemplate.find('h2').addClass('title');
+  imageTemplate.find('img').attr('src', this.url);
+  imageTemplate.find('p').text(this.description);
+  imageTemplate.find('p').addClass('description');
+  imageTemplate.removeClass('template');
+  imageTemplate.addClass(this.keyword);
+  imageTemplate.addClass('objects');
+};
+
+Image.prototype.loadImages = function () {
+  Image.allImages.forEach(img => img.render());
+};
+
+
+Image.readJson = () => {
+  $.get('./data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(item => {
+        Image.allImages.push(new Image(item));
+      });
+    })
+    .then(() => Image.allImages.forEach(img => img.render()))
+    .then(() => Image.allImages[19].options())
+
+};
+
+
+$(Image.readJson());
+
+
